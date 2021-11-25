2a8ac8c95318f1f8344b37edab60bb80aae8b662
kevintofic <79551516+kevintofic@users.noreply.github.com>
2021-11-25 14:37:46 -0300

    Add files via upload

---
+++ type-bolean.js
@@ -0,0 +1,12 @@
+
+
+const PrimeiroNome = 5;
+const SegundoNome = 5;
+let cadastroAtivado = true;
+
+//console.log(PrimeiroNome === SegundoNome)
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
---
+++ type-number.js
@@ -0,0 +1,8 @@
+const texto1 = "ola mundo!";
+const texto2 = 'ola mundo!';
+const senha = "NumeroDeSenha01";
+const StringDeNumeros = "34567";
+
+
+const citação = "meu nome é kevin!";
+console.log(citação)
